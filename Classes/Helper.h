1e9dd5ca38a5d203478d48d2e82925e5a56b9de3
Uffe Koch <uffe@hugelawn.com>
2010-01-30 12:47:53 +0100

    add Helper with static methods

---
+++ Classes/Helper.h
@@ -0,0 +1,18 @@
+//
+//  Helper.h
+//  TrickTheDonkey
+//
+//  Created by Uffe Koch on 30/01/10.
+//  Copyright 2010 Huge Lawn Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Helper : NSObject {
+
+}
+
++ (AVAudioPlayer *)prepAudio:(NSString *)audioFileName;
+
+@end
