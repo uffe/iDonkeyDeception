7286d72357fbe936aa050628b6b8104a20e2d275
Ole Gammelgaard Poulsen <olegammelgaard@gmail.com>
2010-01-30 11:46:13 +0100

    First scene prototype

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -5,6 +5,7 @@
 // HelloWorld Layer
 @interface HelloWorld : CCLayer
 {
+	CCSprite *carrot;
 }
 
 // returns a Scene that contains the HelloWorld as the only child
--- Classes/HelloWorldScene.m
+++ Classes/HelloWorldScene.m
@@ -31,14 +31,28 @@
 	// Apple recommends to re-assign "self" with the "super" return value
 	if( (self=[super init] )) {
 		
+		CGSize size = [[CCDirector sharedDirector] winSize];
+		
+		// addbackground
+		CCSprite *background = [CCSprite spriteWithFile:@"background.png"];
+		background.position = ccp( size.width /2 , size.height/2 );
+		[self addChild:background];
+		
+		// add carrot
+		carrot = [CCSprite spriteWithFile:@"carrot.png"];
+		carrot.position = ccp(200,250);
+		[self addChild:carrot];
+		[self setIsTouchEnabled:YES];
+		
+		
 		// create and initialize a Label
-		CCLabel* label = [CCLabel labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
+		CCLabel* label = [CCLabel labelWithString:@"Trick the Donkey" fontName:@"Marker Felt" fontSize:14];
 
 		// ask director the the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
+
 	
 		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
+		label.position =  ccp( 60, 300 );
 		
 		// add the label as a child to this Layer
 		[self addChild: label];
@@ -46,7 +60,34 @@
 	return self;
 }
 
-- 
+- (BOOL)ccTouchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+	UITouch *touch = [touches anyObject];
+	if (touch) {
+		CGPoint location = [touch locationInView: [touch view]];
+		
+		// IMPORTANT:
+		// The touches are always in "portrait" coordinates. You need to convert them to your current orientation
+		CGPoint convertedPoint = [[CCDirector sharedDirector] convertToGL:location];
+		
+		// we stop the all running actions
+		[carrot stopAllActions];
+		
+		// and we run a new action
+		
+		carrot.position = ccp(convertedPoint.x, carrot.position.y);
+		//[carrot runAction: [CCMoveTo actionWithDuration:0.1 position: ccp(convertedPoint.x, carrot.position.y)]];
+		
+		// no other handlers will receive this event
+		return kEventHandled;
+	}
+	
+	// we ignore the event. Other receivers will receive this event.
+	return kEventHandled;
+	
+}
+
+
+
 
 // on "dealloc" you need to release all your retained objects
 - (void) dealloc
