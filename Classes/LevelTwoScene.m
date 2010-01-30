9d322c9724e5cc0c7c1984367f304ec3f2577b56
Uffe Koch <uffe@hugelawn.com>
2010-01-30 22:57:33 +0100

    introduce the fish

--- Classes/LevelTwoScene.m
+++ Classes/LevelTwoScene.m
@@ -11,8 +11,9 @@
 
 #define L2_CARROT_INITIAL_POS ccp(400,260)
 #define L2_DONKEY_INITIAL_POS ccp(40, 180)
-#define FISH_INITIAL_POS ccp(240, 80)
-#define FISH_MIN_X 105
+#define FISH_INITIAL_POS ccp(400, 30)
+#define FISH_SECOND_POS ccp(240, 80)
+#define FISH_MIN_X 115
 #define FISH_MAX_X 380
 // on "init" you need to initialize your instance
 -(id) init
@@ -52,6 +53,8 @@
 		fish.scaleY = 0.5f;
 		fish.position = FISH_INITIAL_POS;
 		[self addChild:fish];
+		[fish runAction:[CCMoveTo actionWithDuration:5 position:FISH_SECOND_POS]];
+		timeSinceFishFlipped = -5;
 		
 		CCAnimation *fan = [CCAnimation animationWithName:@"fish" delay:0];
 		[fan addFrameWithFilename:@"fishclosed.png"];
