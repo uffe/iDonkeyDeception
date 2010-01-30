1baecf99278a467e44a20ceffda84039d1aaa668
Ole Gammelgaard Poulsen <olegammelgaard@gmail.com>
2010-01-30 10:46:31 +0100

    Initial commit

---
+++ Classes/TrickTheDonkeyAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TrickTheDonkeyAppDelegate.h
+//  TrickTheDonkey
+//
+//  Created by Ole Gammelgaard Poulsen on 30/01/10.
+//  Copyright OGP Consult 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrickTheDonkeyAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TrickTheDonkey
+//
+//  Created by Ole Gammelgaard Poulsen on 30/01/10.
+//  Copyright OGP Consult 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"TrickTheDonkeyAppDelegate");
+	[pool release];
+	return retVal;
+}
