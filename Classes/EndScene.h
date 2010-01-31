420b702d49de2294f5a3a7ad6d1e3e5fb2dfd4fb
Ole Gammelgaard Poulsen <olegammelgaard@gmail.com>
2010-01-31 11:47:01 +0100

    before merge

---
+++ Classes/EndScene.h
@@ -0,0 +1,16 @@
+//
+//  EndScene.h
+//  TrickTheDonkey
+//
+//  Created by Ole Gammelgaard Poulsen on 31/01/10.
+//  Copyright 2010 OGP Consult. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface EndScene : NSObject {
+
+}
+
+@end
---
+++ Classes/EndScene.m
@@ -0,0 +1,14 @@
+//
+//  EndScene.m
+//  TrickTheDonkey
+//
+//  Created by Ole Gammelgaard Poulsen on 31/01/10.
+//  Copyright 2010 OGP Consult. All rights reserved.
+//
+
+#import "EndScene.h"
+
+
+@implementation EndScene
+
+@end
